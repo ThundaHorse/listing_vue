d658bb0b23c8f1bac06d85a85ded2aab3998620b
ThundaHorse <abeeeeekim@gmail.com>
2019-10-18 13:59:31 -0400

    running build for heroku

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from "./store";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://mysterious-cove-60612.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
